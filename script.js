517f39b9957e72b7143f594b3c199aeeaa9b09dd
Alexandr Kruzhanovskyi <alexandrkruz76@gmail.com>
2023-07-02 23:23:19 +0300

    add-cards

--- script.js
+++ script.js
@@ -6,14 +6,30 @@ function fetchPokemonData() {
       .then(data => {
         const results = data.results;
   
+        
+        const pokedexElement = document.getElementById('pokedex');
         results.forEach(pokemon => {
           fetch(pokemon.url)
             .then(response => response.json())
             .then(pokemonData => {
-              console.log(`Pokemon Name: ${pokemonData.name}`);
-              console.log(`Pokemon ID: ${pokemonData.id}`);
-              console.log('--------------');
-            })
+              const card = document.createElement('div');
+                card.className = 'card';
+
+                const image = document.createElement('img');
+                image.src = pokemonData.sprites.front_default;
+
+                const name = document.createElement('h3');
+                name.textContent = pokemonData.name;
+
+                const types = document.createElement('p');
+                types.textContent = 'Types: ' + pokemonData.types.map(type => type.type.name).join(', ');
+
+                card.appendChild(image);
+                card.appendChild(name);
+                card.appendChild(types);
+
+                pokedexElement.appendChild(card);
+              })
             .catch(error => {
               console.log('Error fetching Pokemon data:', error);
             });
