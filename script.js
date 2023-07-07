41fcfbf79576247c4c5329643617df5fcbfb9536
Alexandr Kruzhanovskyi <alexandrkruz76@gmail.com>
2023-07-07 15:07:29 +0300

    fix: add-cards

--- script.js
+++ script.js
@@ -1,44 +1,36 @@
 function fetchPokemonData() {
-    const url = 'https://pokeapi.co/api/v2/pokemon/?limit=12';
-  
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const results = data.results;
-  
-        
-        const pokedexElement = document.getElementById('pokedex');
-        results.forEach(pokemon => {
-          fetch(pokemon.url)
-            .then(response => response.json())
-            .then(pokemonData => {
-              const card = document.createElement('div');
-                card.className = 'card';
+  const url = 'https://pokeapi.co/api/v2/pokemon/?limit=12';
 
-                const image = document.createElement('img');
-                image.src = pokemonData.sprites.front_default;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const results = data.results;
 
-                const name = document.createElement('h3');
-                name.textContent = pokemonData.name;
+      const pokedexElement = document.getElementById('pokedex');
+      let cardsHTML = '';
 
-                const types = document.createElement('p');
-                types.textContent = 'Types: ' + pokemonData.types.map(type => type.type.name).join(', ');
-
-                card.appendChild(image);
-                card.appendChild(name);
-                card.appendChild(types);
-
-                pokedexElement.appendChild(card);
-              })
-            .catch(error => {
-              console.log('Error fetching Pokemon data:', error);
-            });
-        });
-      })
-      .catch(error => {
-        console.log('Error fetching data:', error);
+      results.forEach(pokemon => {
+        fetch(pokemon.url)
+          .then(response => response.json())
+          .then(pokemonData => {
+            const cardHTML = `
+              <div class="card">
+                <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}" />
+                <h3>${pokemonData.name}</h3>
+                <p>Types: ${pokemonData.types.map(type => type.type.name).join(', ')}</p>
+              </div>`;
+              
+            cardsHTML += cardHTML;
+            pokedexElement.innerHTML = cardsHTML;
+          })
+          .catch(error => {
+            console.log('Error fetching Pokemon data:', error);
+          });
       });
-  }
-  
-  fetchPokemonData();
-  
+    })
+    .catch(error => {
+      console.log('Error fetching data:', error);
+    });
+}
+
+fetchPokemonData();
